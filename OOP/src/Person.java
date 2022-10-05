dad06116568c4d393b3aa72cd7a66efdc73323d8
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-05 15:56:39 -0500

    completed and tested exercise - Person class

--- OOP/src/Person.java
+++ OOP/src/Person.java
@@ -1,3 +1,4 @@
+
 public class Person {
 // Write a class with the name Person. The class needs three fields (instance variables) with the names firstName, lastName of type String and age of type int.
 private String firstName;
@@ -5,7 +6,7 @@ private String lastName;
 private int age;
 
 // Method named getFirstName without any parameters, it needs to return the value of the firstName field.
-public String getFirstString() {
+public String getFirstName() {
     return this.firstName;
 }
 
@@ -38,14 +39,41 @@ public void setAge(int age) {
 }
 
 // Method named isTeen without any parameters, it needs to return true if the value of the age field is greater than 12 and less than 20, otherwise, return false.
+public boolean isTeen() {
+    if (this.age > 12 && this.age < 20) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Method named getFullName without any parameters, it needs to return the full name of the person.
+public String getFullName() {
 
-// In case both firstName and lastName fields are empty, Strings return an empty String.
-
-// In case lastName is an empty String, return firstName.
-
-// In case firstName is an empty String, return lastName.
-
-// To check if s String is empty, use the method isEmpty from the String class. For example,firstName.isEmpty() returns true if the String is empty or in other words, when the String does not contain any characters.
+    // In case both firstName and lastName fields are empty, Strings return an empty String.
+    if(this.firstName.isEmpty() && this.lastName.isEmpty()) {
+        return " ";
+    // In case lastName is an empty String, return firstName.
+    } else if (this.lastName.isEmpty()) {
+        return this.firstName;
+    // In case firstName is an empty String, return lastName.
+    } else if (this.firstName.isEmpty()) {
+        return this.lastName;
+    }
+    return this.firstName + " " + this.lastName;
+}
+public static void main(String[] args) {
+    Person person = new Person();
+    person.setFirstName("");   
+    person.setLastName("");    
+    person.setAge(10);
+    System.out.println("Full name: " + person.getFullName());
+    System.out.println("Is a teen: " + person.isTeen());
+    person.setFirstName("John");    
+    person.setAge(18);
+    System.out.println("Full name: " + person.getFullName());
+    System.out.println("Is a teen: " + person.isTeen());
+    person.setLastName("Smith");    
+    System.out.println("Full name: " + person.getFullName());
+}
 }

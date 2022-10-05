519e9b095a0f7441847d87d1985330c7f85f6643
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-05 12:18:28 -0500

    created SimpleCalculator class

---
+++ OOP/src/SimpleCalculator.java
@@ -0,0 +1,14 @@
+public class SimpleCalculator {
+    // Write a class with the name SimpleCalculator. The class needs two fields (instance variables) with names firstNumber and secondNumber both of type double.
+
+    // Write the following methods (instance methods):
+
+    // Method named getFirstNumber without any parameters, it needs to return the value of firstNumber field.
+
+    // Method named getSecondNumber without any parameters, it needs to return the value of secondNumber field.
+
+    // Method named setFirstNumber with one parameter of type double, it needs to set the value of the firstNumber field.
+
+    // Method named setSecondNumber with one parameter of type double, it needs to set the value of the secondNumberfield.
+}
+

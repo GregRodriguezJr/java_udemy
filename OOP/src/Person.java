3444030d3e212b282a658ef92737e567b6417985
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-05 15:18:12 -0500

    added setters/getters for Person class

--- OOP/src/Person.java
+++ OOP/src/Person.java
@@ -5,16 +5,37 @@ private String lastName;
 private int age;
 
 // Method named getFirstName without any parameters, it needs to return the value of the firstName field.
+public String getFirstString() {
+    return this.firstName;
+}
 
 // Method named getLastName without any parameters, it needs to return the value of the lastName field.
+public String getLastname() {
+    return this.lastName;
+}
 
 // Method named getAge without any parameters, it needs to return the value of the age field.
+public int getAge() {
+    return this.age;
+}
 
 // Method named setFirstName with one parameter of type String, it needs to set the value of the firstName field.
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
 
 // Method named setLastName with one parameter of type String, it needs to set the value of the lastName field.
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
 
 // Method named setAge with one parameter of type int, it needs to set the value of the age field. If the parameter is less than 0 or greater than 100, it needs to set the age field value to 0.
+public void setAge(int age) {
+    if (age < 0 || age > 100) {
+        this.age = 0;
+    }
+    this.age = age;
+}
 
 // Method named isTeen without any parameters, it needs to return true if the value of the age field is greater than 12 and less than 20, otherwise, return false.
 

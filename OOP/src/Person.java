ee400a05b33d01e50d7f0f65a3cd9ca69d5e807e
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-05 16:48:27 -0500

    created new BankAccount class with fields

--- OOP/src/Person.java
+++ OOP/src/Person.java
@@ -62,18 +62,18 @@ public String getFullName() {
     }
     return this.firstName + " " + this.lastName;
 }
-public static void main(String[] args) {
-    Person person = new Person();
-    person.setFirstName("");   
-    person.setLastName("");    
-    person.setAge(10);
-    System.out.println("Full name: " + person.getFullName());
-    System.out.println("Is a teen: " + person.isTeen());
-    person.setFirstName("John");    
-    person.setAge(18);
-    System.out.println("Full name: " + person.getFullName());
-    System.out.println("Is a teen: " + person.isTeen());
-    person.setLastName("Smith");    
-    System.out.println("Full name: " + person.getFullName());
-}
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");   
+        person.setLastName("");    
+        person.setAge(10);
+        System.out.println("Full name: " + person.getFullName());
+        System.out.println("Is a teen: " + person.isTeen());
+        person.setFirstName("John");    
+        person.setAge(18);
+        System.out.println("Full name: " + person.getFullName());
+        System.out.println("Is a teen: " + person.isTeen());
+        person.setLastName("Smith");    
+        System.out.println("Full name: " + person.getFullName());
+    }
 }

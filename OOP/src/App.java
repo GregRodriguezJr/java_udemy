c2e01e2a21bf4d30e26f55ba6271ba0e4b94d749
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-05 17:34:43 -0500

    created getters/setters for BankAccount class

--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Test BankAccount output
+        // BankAccount account123 = new BankAccount();
+        // account123.setName("John Doe");
 
+        // System.out.println(account123.getName());
     }
 }
--- OOP/src/BankAccount.java
+++ OOP/src/BankAccount.java
@@ -1,17 +1,47 @@
+
 public class BankAccount {
     // Create fields for the account number, balance, customer name, email and phone number.
-    int accountNum;
-    int balance;
-    String name;
-    String email;
-    String phoneNum;
+    private long accountNum;
+    private long balance;
+    private String name;
+    private String email;
+    private String phoneNum;
 
     // Create getters and setters for each field
+    // Getters
+    public long getAccountNum() {
+        return this.accountNum;
+    }
+    public long getBalance() {
+        return this.balance;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPhoneNum() {
+        return this.phoneNum;
+    }
+    // Setters
+    public void setAccountNum(long accountNum) {
+        this.accountNum = accountNum;
+    }
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
     // Create two additional methods
     // 1. To allow the customer to deposit funds (this should increment the balance field).
     // 2. To allow the customer to withdraw funds. This should deduct from the balance field,
     // but not allow the withdrawal to complete if their are insufficient funds.
-    // You will want to create various code in the Main class (the one created by IntelliJ) to
-    // confirm your code is working.
-    // Add some System.out.println's in the two methods above as well.
 }

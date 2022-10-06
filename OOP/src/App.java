110134670a392632f42d4545cadf293eb5089467
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-06 11:38:06 -0500

    added constructor and completed exercise

--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Test BankAccount output
-        BankAccount account123 = new BankAccount();
-        account123.setName("John Doe");
-        account123.setBalance(3500);
-        System.out.println(account123.getBalance());
-        account123.withdraw(600);
+        BankAccount newMember = new BankAccount("1234", 0, "John Doe", "johndoe@gmail.com", "555-555-5555");
+
+        newMember.withdraw(50);
+        newMember.deposit(100);
+        newMember.withdraw(10);
+        System.out.println(newMember.getAccountNum());
     }
 }
--- OOP/src/BankAccount.java
+++ OOP/src/BankAccount.java
@@ -1,15 +1,24 @@
 
 public class BankAccount {
     // Create fields for the account number, balance, customer name, email and phone number.
-    private long accountNum;
+    private String accountNum;
     private long balance;
     private String name;
     private String email;
     private String phoneNum;
 
+    // Constructor to add user information
+    public BankAccount(String accountNum, long balance, String name, String email, String phoneNum) {
+        this.accountNum = accountNum;
+        this.balance = balance;
+        this.name = name;
+        this.email = email;
+        this.phoneNum = phoneNum;
+    }
+
     // Create getters and setters for each field
     // Getters
-    public long getAccountNum() {
+    public String getAccountNum() {
         return this.accountNum;
     }
     public long getBalance() {
@@ -25,7 +34,7 @@ public class BankAccount {
         return this.phoneNum;
     }
     // Setters
-    public void setAccountNum(long accountNum) {
+    public void setAccountNum(String accountNum) {
         this.accountNum = accountNum;
     }
     public void setBalance(long balance) {
@@ -43,12 +52,16 @@ public class BankAccount {
     // To allow the customer to deposit funds (this should increment the balance field).
     public void deposit(long depositAmount) {
         this.balance += depositAmount;
-        System.out.println("Your new account balance is: " + this.balance);
+        System.out.println("Deposit of $" + depositAmount + " successful, your new account balance is: $" + this.balance);
     }
     // To allow the customer to withdraw funds. This should deduct from the balance field,
     // but not allow the withdrawal to complete if their are insufficient funds.
     public void withdraw(long withdrawAmount) {
-        this.balance -= withdrawAmount;
-        System.out.println("Your new accout balance is: " + this.balance);
+        if(this.balance - withdrawAmount < 0) {
+            System.out.println("Insufficient funds, current balance is: $" + this.balance + ", can not withdraw $" + withdrawAmount);
+        } else {
+            this.balance -= withdrawAmount;
+            System.out.println("Withdraw of $" + withdrawAmount + " successful, your new accout balance is: $" + this.balance);
+        }
     }
 }

612724577690a205496563d55ba28447260a3aea
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-18 10:39:02 -0500

    created new package, completed primitive challenge

---
+++ src/firstSteps/Main.java
@@ -0,0 +1,17 @@
+package firstSteps;
+
+public class Main {
+    public static void main(String[] args) {
+        // Primitive Type Challenge
+        byte byteNum = 115;
+        short shortNum = 30;
+        int intNum = 100;
+        long longNum = 50_000L + 10L * (byteNum + shortNum + intNum);
+        System.out.println(longNum);
+        // Max and min values
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        System.out.println("Integer max value is: " + maxInt);
+        System.out.println("Integer min value is: " + minInt);
+    }
+}

eeffd3de00b2087d872691b4c1d815fcadb4a311
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-18 11:00:11 -0500

    completed floating point challenge

--- src/firstSteps/Main.java
+++ src/firstSteps/Main.java
@@ -3,15 +3,22 @@ package firstSteps;
 public class Main {
     public static void main(String[] args) {
         // Primitive Type Challenge
-        byte byteNum = 115;
-        short shortNum = 30;
-        int intNum = 100;
-        long longNum = 50_000L + 10L * (byteNum + shortNum + intNum);
-        System.out.println(longNum);
+//        byte byteNum = 115;
+//        short shortNum = 30;
+//        int intNum = 100;
+//        long longNum = 50_000L + 10L * (byteNum + shortNum + intNum);
+//        System.out.println(longNum);
+
         // Max and min values
-        int maxInt = Integer.MAX_VALUE;
-        int minInt = Integer.MIN_VALUE;
-        System.out.println("Integer max value is: " + maxInt);
-        System.out.println("Integer min value is: " + minInt);
+//        int maxInt = Integer.MAX_VALUE;
+//        int minInt = Integer.MIN_VALUE;
+//        System.out.println("Integer max value is: " + maxInt);
+//        System.out.println("Integer min value is: " + minInt);
+
+        // Floating point precision challenge
+        double pounds = 155;
+        double lbsToKilo = 0.45359237;
+        double kiloResult = pounds * lbsToKilo;
+        System.out.println(pounds + " lbs is equal to: " + kiloResult);
     }
 }

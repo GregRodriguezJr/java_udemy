6ff8b9e0d452bec1acc57462fd0500dec3abfda0
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-24 14:15:24 -0600

    created linkedList class and main class with variables and methods

---
+++ src/DSA/linkedList/LinkedList.java
@@ -0,0 +1,33 @@
+package DSA.linkedList;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    //  Inner/nested class
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}

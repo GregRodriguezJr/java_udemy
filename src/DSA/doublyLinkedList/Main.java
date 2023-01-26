36906ce838c21cfba1e8b2fcf00032dec9d067a1
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-26 09:15:13 -0600

    created doublyLinked class and tested constructor

---
+++ src/DSA/doublyLinkedList/Main.java
@@ -0,0 +1,14 @@
+package DSA.doublyLinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Test constructor for DDL
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+        myDLL.printList();
+    }
+}

c5e8a5760f878b81cf22361b57ed2d5c9a67c8c8
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-26 12:45:16 -0600

    created and tested remove first method

--- src/DSA/doublyLinkedList/Main.java
+++ src/DSA/doublyLinkedList/Main.java
@@ -26,10 +26,20 @@ public class Main {
 //        // 0 Items - Returns null
 //        System.out.println(myDLL.removeLast());
 
-        // Test prepend method
+//        // Test prepend method
+//        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+//        myDLL.append(3);
+//        myDLL.prepend(1);
+//        myDLL.printList();
+
+        // Test Remove first method
         DoublyLinkedList myDLL = new DoublyLinkedList(2);
-        myDLL.append(3);
-        myDLL.prepend(1);
-        myDLL.printList();
+        myDLL.append(1);
+        // 2 Items - Returns 2 node
+        System.out.println(myDLL.removeFirst().value);
+        // 1 Items - Returns 1 node
+        System.out.println(myDLL.removeFirst().value);
+        // 0 Items - Returns null
+        System.out.println(myDLL.removeFirst());
     }
 }

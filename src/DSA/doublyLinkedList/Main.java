1fbc20c6911b20dec3aeb7c6f293629228081948
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-26 12:13:28 -0600

    created and tested prepend method

--- src/DSA/doublyLinkedList/Main.java
+++ src/DSA/doublyLinkedList/Main.java
@@ -16,14 +16,20 @@ public class Main {
 //        myDLL.append(2);
 //        myDLL.printList();
 
-        // Test remove last method
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.append(2);
-        // 2 Items - Returns 2 node
-        System.out.println(myDLL.removeLast().value);
-        // 1 Items - Returns  node
-        System.out.println(myDLL.removeLast().value);
-        // 0 Items - Returns null
-        System.out.println(myDLL.removeLast());
+//        // Test remove last method
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.append(2);
+//        // 2 Items - Returns 2 node
+//        System.out.println(myDLL.removeLast().value);
+//        // 1 Items - Returns  node
+//        System.out.println(myDLL.removeLast().value);
+//        // 0 Items - Returns null
+//        System.out.println(myDLL.removeLast());
+
+        // Test prepend method
+        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+        myDLL.append(3);
+        myDLL.prepend(1);
+        myDLL.printList();
     }
 }

de5c397ddaa4c4a322b5fa45826df3dc8acd3761
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-26 12:57:01 -0600

    added and tested get method

--- src/DSA/doublyLinkedList/Main.java
+++ src/DSA/doublyLinkedList/Main.java
@@ -32,14 +32,23 @@ public class Main {
 //        myDLL.prepend(1);
 //        myDLL.printList();
 
-        // Test Remove first method
-        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+//        // Test Remove first method
+//        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+//        myDLL.append(1);
+//        // 2 Items - Returns 2 node
+//        System.out.println(myDLL.removeFirst().value);
+//        // 1 Items - Returns 1 node
+//        System.out.println(myDLL.removeFirst().value);
+//        // 0 Items - Returns null
+//        System.out.println(myDLL.removeFirst());
+
+        // Test get method
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
         myDLL.append(1);
-        // 2 Items - Returns 2 node
-        System.out.println(myDLL.removeFirst().value);
-        // 1 Items - Returns 1 node
-        System.out.println(myDLL.removeFirst().value);
-        // 0 Items - Returns null
-        System.out.println(myDLL.removeFirst());
+        myDLL.append(2);
+        myDLL.append(3);
+        System.out.println(myDLL.get(1).value);
+        System.out.println(myDLL.get(2).value + "\n");
+        myDLL.printList();
     }
 }

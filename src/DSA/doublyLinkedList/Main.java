48f2cb42a79d36e9eeec33937b6463321e11a41b
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-26 12:02:10 -0600

    created remove last method and tested

--- src/DSA/doublyLinkedList/Main.java
+++ src/DSA/doublyLinkedList/Main.java
@@ -11,9 +11,19 @@ public class Main {
 //        myDLL.getLength();
 //        myDLL.printList();
 
-        // Test append method
+//        // Test append method
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+//        myDLL.append(2);
+//        myDLL.printList();
+
+        // Test remove last method
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
         myDLL.append(2);
-        myDLL.printList();
+        // 2 Items - Returns 2 node
+        System.out.println(myDLL.removeLast().value);
+        // 1 Items - Returns  node
+        System.out.println(myDLL.removeLast().value);
+        // 0 Items - Returns null
+        System.out.println(myDLL.removeLast());
     }
 }

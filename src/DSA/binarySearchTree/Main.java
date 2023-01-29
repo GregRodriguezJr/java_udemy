b8d99678dde34d2ceaeefde981cd775df1a39ddb
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-29 13:19:28 -0600

    created and tested insert method

--- src/DSA/binarySearchTree/Main.java
+++ src/DSA/binarySearchTree/Main.java
@@ -5,6 +5,18 @@ public class Main {
 
         BinarySearchTree myBST = new BinarySearchTree();
 
-        System.out.println("root = " + myBST.root);
+//        System.out.println("root = " + myBST.root);
+
+        // Test insert method
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(70);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+        System.out.println(myBST.root.left.right.value);
+
     }
 }

7b81c61134d444fcd58b617f2a817613dfe1a6e9
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-29 11:08:42 -0600

    created binary search tree package and class with constructor

---
+++ src/DSA/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package DSA.binarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
---
+++ src/DSA/binarySearchTree/Main.java
@@ -0,0 +1,10 @@
+package DSA.binarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("root = " + myBST.root);
+    }
+}

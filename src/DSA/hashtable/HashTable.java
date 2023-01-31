0f23a0e88a350b9ee93531aae834e9a7da35e9ef
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-31 13:05:31 -0600

    created and tested set method

--- src/DSA/hashtable/HashTable.java
+++ src/DSA/hashtable/HashTable.java
@@ -19,8 +19,32 @@ public class HashTable {
         dataMap = new Node[size];
     }
 
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key,value);
+        if(dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
     public void printTable() {
-        for(int i = 0; i < dataMap.length; i++) {
+        for (int i = 0; i < dataMap.length; i++) {
             System.out.println(i + ":");
             Node temp = dataMap[i];
             while (temp != null) {

7756cb11fb9306aac26cf2df6b69957464806d82
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-31 13:20:47 -0600

    created and tested get method

--- src/DSA/hashtable/Main.java
+++ src/DSA/hashtable/Main.java
@@ -4,12 +4,18 @@ public class Main {
     public static void main(String[] args) {
         HashTable myHashTable = new HashTable();
 
+//        // test set method
+//        myHashTable.set("bolts", 200);
+//        myHashTable.set("screws", 140);
+//        myHashTable.printTable();
+
+        // test get method
         myHashTable.set("nails", 100);
         myHashTable.set("tile", 50);
         myHashTable.set("lumber", 80);
-        myHashTable.set("bolts", 200);
-        myHashTable.set("screws", 140);
-
         myHashTable.printTable();
+        System.out.println("---------------");
+        System.out.println(myHashTable.get("lumber"));
+        System.out.println(myHashTable.get("bolts"));
     }
 }

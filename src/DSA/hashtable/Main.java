fde2a7da1334af92ff3ee3d8c8072fbe3f5f471f
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-31 12:28:01 -0600

    created hastable dir and class with contructor and methods

---
+++ src/DSA/hashtable/Main.java
@@ -0,0 +1,9 @@
+package DSA.hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+
+        myHashTable.printTable();
+    }
+}

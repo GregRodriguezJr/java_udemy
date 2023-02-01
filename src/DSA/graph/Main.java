01a15159c4be9695b8136d391c5e550573f624f8
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-01 09:04:19 -0600

    created graph dir and class

---
+++ src/DSA/graph/Main.java
@@ -0,0 +1,12 @@
+package DSA.graph;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+
+        myGraph.printGraph();
+    }
+}

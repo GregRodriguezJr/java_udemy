d0ff5203545bcd83c1353c8e2afa7f6083c758c7
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-07 09:27:23 -0600

    created and tested mergesort method

--- src/DSA/sorts/mergesort/MergeSort.java
+++ src/DSA/sorts/mergesort/MergeSort.java
@@ -37,12 +37,30 @@ public class MergeSort {
         return combined;
     }
 
+    public static int[] mergeSort(int[] array) {
+        if (array.length == 1) return array;
+
+        int midIndex = array.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+        int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+
+        return merge(left, right);
+    }
+
 
     public static void main(String[] args) {
-        int[] array1 = {1,3,7,8};
-        int[] array2 = {2,4,5,6};
-        System.out.println(Arrays.toString(array1));
-        System.out.println(Arrays.toString(array2));
-        System.out.println(Arrays.toString(merge(array1,array2)));
+//        // Test merge method
+//        int[] array1 = {1,3,7,8};
+//        int[] array2 = {2,4,5,6};
+//        System.out.println(Arrays.toString(array1));
+//        System.out.println(Arrays.toString(array2));
+//        System.out.println(Arrays.toString(merge(array1,array2)));
+
+        // Test merge sort method
+        int[] originalArray = {3,1,4,2};
+        int[] sortedArray = mergeSort(originalArray);
+
+        System.out.println("\nOriginal Array: " + Arrays.toString(originalArray));
+        System.out.println("\nSorted Array: " + Arrays.toString(sortedArray));
     }
 }

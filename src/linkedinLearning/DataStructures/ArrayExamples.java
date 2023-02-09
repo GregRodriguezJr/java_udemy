6a68b556624b04843f7d446f777db1ca4c6d3bc8
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-09 08:24:01 -0600

    created class with array examples

---
+++ src/linkedinLearning/DataStructures/ArrayExamples.java
@@ -0,0 +1,29 @@
+package linkedinLearning.DataStructures;
+
+import java.util.Arrays;
+
+public class ArrayExamples {
+
+    public static void main(String[] args) {
+
+        String[] weekdays = new String[7];
+        System.out.println(Arrays.toString(weekdays));
+
+        weekdays[0] = "Monday";
+        System.out.println(Arrays.toString(weekdays));
+
+        weekdays = new String[]{"Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"};
+        System.out.println(Arrays.toString(weekdays));
+
+        for (String day : weekdays) {
+            System.out.println(day);
+        }
+
+        for (int i = 0; i < weekdays.length; i++) {
+            weekdays[i] = weekdays[i].toUpperCase();
+        }
+        System.out.println(Arrays.toString(weekdays));
+
+    }
+
+}

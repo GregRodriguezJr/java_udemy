9ae9a0fc50ac6b1c34ab3caecf4139a364a1a908
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-09 21:48:45 -0600

    completed linkedlist methods

--- src/linkedinLearning/DataStructures/CollectionsExercise.java
+++ src/linkedinLearning/DataStructures/CollectionsExercise.java
@@ -7,12 +7,12 @@ import java.util.Set;
 
 public class CollectionsExercise {
 
-    void addToEndOfLinkedList(LinkedList<Integer> numbers, int i) {
-
+    static void addToEndOfLinkedList(LinkedList<Integer> numbers, int i) {
+        numbers.add(i);
     }
 
-    void addToStartOfLinkedList(LinkedList<Integer> numbers, int i) {
-
+    static void addToStartOfLinkedList(LinkedList<Integer> numbers2, int i) {
+        numbers2.addFirst(i);
     }
 
     void removeItemFromTopOfStack(Deque<Integer> stack) {
@@ -29,6 +29,23 @@ public class CollectionsExercise {
 
     public static void main(String[] args) {
 
+        // Test addToEndOfLinkedList
+        LinkedList<Integer> numbers = new LinkedList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println("Before add: " + numbers);
+        addToEndOfLinkedList(numbers,4);
+        System.out.println("After add: " + numbers);
+        System.out.println("\n------------------------\n");
 
+        // Test addToStartOfLinkedList
+        LinkedList<Integer> numbers2 = new LinkedList<>();
+        numbers2.add(2);
+        numbers2.add(3);
+        numbers2.add(4);
+        System.out.println("Before addFirst: " + numbers2);
+        addToStartOfLinkedList(numbers2,1);
+        System.out.println("After addFirst: " + numbers2);
     }
 }
